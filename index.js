bc46fbca9dc87647892ec3d2bd3bc38d4ed0dfab
BECCA LEE <REBECCA.KIRSTIN.LEE@GMAIL.COM>
2019-01-08 15:45:25 -0800

    Starts on code challenge

--- index.js
+++ index.js
@@ -5,3 +5,12 @@ const server = require('./server.js');
 const port = process.env.PORT || 8080;
 
 server.start(port);
+
+
+// 'use strict';
+
+// const server = require('./server.js');
+
+// const port = process.env.PORT || 8080;
+
+// server.start(port);
--- server.js
+++ server.js
@@ -1,54 +1,55 @@
 'use strict';
 
-//these are the typical names for these dependencies
+//Dependencies
 const express = require('express');
 const app = express();
 
-// EJS template setup
-app.set('views', `${__dirname}/views`); //this provides the full server path
-app.set('view engine', 'ejs'); //this sets you up to do ejs templates. This line of code acts as a way to set the dependency for EJS
+//EJS template setup
+app.set('views', `${__dirname}/views`);
+app.set('view engine', 'ejs');
 
-// Static Routes
+//Static Routes
 app.use(express.static(`${__dirname}/public`));
 
-// App Middleware
-app.use(express.json()); //if you send a json object to the server in a post, this will parse the object and let you use it
+//App Middleware
+app.use(express.json());
 
-// Routes
-app.get('/', (req, res)=> {
-  res.send('<h1>Hello From/</h1>');
+//Routes
+app.get('/', (req, res) => {
+  res.send('<h1>Hello From</h1>');
 });
 
-app.post('/save', (req, res) => { //if you send a json object in it will send one back
-  res.json(req.body);
+app.post('/save', (req, res) => {
+  req.json(req.body);
 });
 
 app.get('/err', (req, res, next) => {
-  next('This is a catastrophic error'); //If there is middleware, and you call next with something, it'll create a catastrophic error
+  next('This is a catastrophic error');
 });
 
-// 404 handling
+//404 Handling
 app.get('*', (req, res) => {
-  res.status(404);
+  res.status('404');
   res.statusMessage = 'Not Found';
-  res.render('not-found', {request: req}); // render will render and ejs file. Send would not!
+  res.render('not-found', {request: req});
 });
 
-// Error handling
-app.use((err, req, res, next) => { // express knows this will be an error message based on the params
+//Error Handling
+app.use((err, req, res, next) => {
   res.status(500);
   res.statusMessage = 'Server Error';
-  res.render('error', {request: req, error:err}); 
+  res.render('error', {request: req, error:err});
 });
 
 module.exports = {
-  server: app, 
+  server: app,
   start: (port) => {
     app.listen(port, () => console.log('Server up on port', port));
   },
-};
+}
 
-// app.listen(8081, () => console.log('Server up on 8081'));
+app.listen(8081, () => console.log('Server up on 8081'));
+
 
 // to run in terminal, run nodemon, then enter `http http://localhost:8080/`. Add file names on at the end if you want. Can also be run as `http :8080`
 
