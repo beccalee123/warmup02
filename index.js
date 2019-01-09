49092ff7ef53438ed4b96da5e2d94b293d11440f
BECCA LEE <REBECCA.KIRSTIN.LEE@GMAIL.COM>
2019-01-08 21:08:08 -0800

    updates index

--- index.js
+++ index.js
@@ -5,12 +5,3 @@ const server = require('./server.js');
 const port = process.env.PORT || 8080;
 
 server.start(port);
-
-
-// 'use strict';
-
-// const server = require('./server.js');
-
-// const port = process.env.PORT || 8080;
-
-// server.start(port);
